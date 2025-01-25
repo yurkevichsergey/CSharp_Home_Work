79fc2cfa712d501e7b61b771b2dfa4bd06b694db
yurkevichsergey <sergey.yurkevich.93@gmail.com>
2025-01-25 22:45:07 +0200

    Update Program.cs

--- Task_5.2/Program.cs
+++ Task_5.2/Program.cs
@@ -7,9 +7,13 @@ try
     correct_number_1 = double.Parse(number_1);
     Console.WriteLine($"\nYour number is correct!");
 }
+catch (FormatException)
+{
+    Console.WriteLine("Error, you enter not a number");
+}
 catch (Exception)
 {
-    Console.WriteLine("\nInvalid enter. Please enter the numbers, not symbols");
+    Console.WriteLine("\nInvalid enter. Please enter the numbers");
 }
 finally
 {
